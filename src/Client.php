6531fc28f162393d2cfa834d8a9bf36af4a0cdb3
Diego Rivas <diegorivas89@gmail.com>
2018-02-22 12:39:51 -0300

    Add products client

--- src/Client.php
+++ src/Client.php
@@ -4,23 +4,23 @@ namespace WoowUp;
 use WoowUp\Endpoints\Purchases;
 use WoowUp\Endpoints\Users;
 
-class Client {
-	const HOST = 'https://api.woowup.com';
-	const VERSION = 'apiv3';
+class Client
+{
+    const HOST    = 'https://api.woowup.com';
+    const VERSION = 'apiv3';
 
-	protected $http;
+    protected $http;
 
-	public $purchases;
-	public $mailings;
-	public $users;
-	public $segments;
-	public $products;
+    public $purchases;
+    public $mailings;
+    public $users;
+    public $segments;
+    public $products;
 
-	function __construct($apikey)
-	{
-		$this->purchases = new Purchases(self::HOST.'/'.self::VERSION, $apikey);
-		$this->users = new Users(self::HOST.'/'.self::VERSION, $apikey);
-	}
+    public function __construct($apikey)
+    {
+        $this->purchases = new Purchases(self::HOST . '/' . self::VERSION, $apikey);
+        $this->users     = new Users(self::HOST . '/' . self::VERSION, $apikey);
+        $this->products  = new Products(self::HOST . '/' . self::VERSION, $apikey);
+    }
 }
-
-?>
